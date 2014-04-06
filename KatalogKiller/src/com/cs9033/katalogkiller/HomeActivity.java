915423c68c29692e2f81a700946080e9e70f3fd3
Shilpan Patel <skp344@nyu.edu>
2014-04-05 20:52:58 -0400

    Home screen fixes.

--- KatalogKiller/src/com/cs9033/katalogkiller/HomeActivity.java
+++ KatalogKiller/src/com/cs9033/katalogkiller/HomeActivity.java
@@ -3,17 +3,77 @@ package com.cs9033.katalogkiller;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class HomeActivity extends Activity {
-	
+
 	private static String TAG = "HomeActivity";
-	
+
+	private Button cameraButton;
+	private Button catalogButton;
+	private Button faqButton;
+	private Button leadeBoardButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
+
+		bindComponents();
+		addListeners();
 	}
-	
+
+	private void bindComponents() {
+
+		cameraButton = (Button) findViewById(R.id.camera_button);
+		catalogButton = (Button) findViewById(R.id.catalog_button);
+		faqButton = (Button) findViewById(R.id.faq_button);
+		leadeBoardButton = (Button) findViewById(R.id.leaderboard_button);
+
+	}
+
+	private void addListeners() {
+
+		cameraButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		catalogButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		faqButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		leadeBoardButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
