8ccc4cb3254986757df698aabc01534782ff069b
namanpratyush <np1225@nyu.edu>
2014-04-05 21:30:10 -0400

    Register Page

---
+++ KatalogKiller/src/com/cs9033/katalogkiller/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.cs9033.katalogkiller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterActivity extends Activity {
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
