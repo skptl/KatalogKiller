47d82a2df64fe768d44ab7fc436f7e0eaff1594a
Shilpan Patel <skp344@nyu.edu>
2014-04-19 20:38:47 -0400

    Android NDK fixes for compiling non-free libraries in OpenCV

--- KatalogKiller/jni/Android.mk
+++ KatalogKiller/jni/Android.mk
@@ -1,16 +1,23 @@
-# Compile libnonfree.so
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 OPENCV_INSTALL_MODULES:=on
-OPENCV_CAMERA_MODULES:=on
-include /Users/CoRrUpT/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/OpenCV.mk
+OPENCV_CAMERA_MODULES:=off
 
-LOCAL_C_INCLUDES:= /Users/CoRrUpT/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include
+#export OPENCV_PACKAGE_DIR=/Users/CoRrUpT/Downloads/OpenCV-2.4.8-android-sdk
+
+include $(OPENCV_PACKAGE_DIR)/sdk/native/jni/OpenCV.mk
+
+LOCAL_C_INCLUDES += $(OPENCV_PACKAGE_DIR)/sdk/native/jni/include
 LOCAL_MODULE    := nonfree
 LOCAL_CFLAGS    := -Werror -O3 -ffast-math
 LOCAL_LDLIBS    += -llog
-LOCAL_SRC_FILES := nonfree_init.cpp \
-		precomp.cpp \
-		sift.cpp \
-		surf.cpp
+
+# for 2.4.8, delete the line precomp.cpp \
+LOCAL_SRC_FILES := cuda/surf.cu \
+opencl/surf.cl \
+nonfree_init.cpp \
+sift.cpp \
+surf_gpu.cpp \
+surf.cpp \
+surf.ocl.cpp
 include $(BUILD_SHARED_LIBRARY)
--- KatalogKiller/jni/Application.mk
+++ KatalogKiller/jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := all 
+APP_ABI := all
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -fexceptions
-APP_PLATFORM := android-19
+APP_PLATFORM := android-8
