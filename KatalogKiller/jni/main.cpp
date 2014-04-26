0407006d1627e93be434542642637ea0665897b2
Shilpan Patel <skp344@nyu.edu>
2014-04-26 15:16:52 -0400

    SIFT enhancements.

--- KatalogKiller/jni/main.cpp
+++ KatalogKiller/jni/main.cpp
@@ -6,8 +6,16 @@
 #include <opencv2/features2d/features2d.hpp>
 #include <vector>
 
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <iostream>
+
 using namespace std;
 using namespace cv;
+
+#define  LOG_TAG    "main.cpp"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 
 extern "C" {
 	JNIEXPORT void JNICALL Java_com_cs9033_katalogkiller_ScannerActivity_findFeatures(JNIEnv*, jobject, jlong addrGray, jlong addrRgba);
@@ -17,14 +25,27 @@ JNIEXPORT void JNICALL Java_com_cs9033_katalogkiller_ScannerActivity_findFeature
 {
     Mat& mGr  = *(Mat*)addrGray;
     Mat& mRgb = *(Mat*)addrRgba;
-    vector<KeyPoint> v;
+    vector<KeyPoint> keypoints;
+    Mat descriptors;
 
-    FastFeatureDetector detector(50);
-    detector.detect(mGr, v);
-    for( unsigned int i = 0; i < v.size(); i++ )
+	// Create a SIFT keypoint detector.
+	SiftFeatureDetector detector;
+	detector.detect(mGr, keypoints);
+	LOGI("Detected %d keypoints\n", (int) keypoints.size());
+
+	// Compute feature description.
+	detector.compute(mGr,keypoints, descriptors);
+	LOGI("Compute feature.\n");
+
+    //FastFeatureDetector detector(50);
+    //detector.detect(mGr, v);
+    /*for( unsigned int i = 0; i < keypoints.size(); i++ )
     {
-        const KeyPoint& kp = v[i];
-        circle(mRgb, Point(kp.pt.x, kp.pt.y), 10, Scalar(255,0,0,255));
-    }
+        const KeyPoint& kp = keypoints[i];
+        Scalar keypointColor = Scalar(255, 0, 0);
+        circle(mRgb, Point(kp.pt.x, kp.pt.y), 10, keypointColor);
+    }*/
+	Scalar keypointColor = Scalar(255, 0, 0);
+	drawKeypoints(mGr, keypoints, mRgb, keypointColor, DrawMatchesFlags::DRAW_OVER_OUTIMG);
 }
 
