77daf7077f0e51ee97dccf8bc4ebceb67c66b325
Shilpan Patel <skp344@nyu.edu>
2014-04-27 18:40:09 -0400

    MIME support added for uploading images.

--- KatalogKiller/src/com/cs9033/katalogkiller/utilities/HttpCommunicator.java
+++ KatalogKiller/src/com/cs9033/katalogkiller/utilities/HttpCommunicator.java
@@ -2,16 +2,31 @@ package com.cs9033.katalogkiller.utilities;
 
 import java.util.concurrent.Callable;
 
+import org.apache.http.HttpVersion;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
+
 public class HttpCommunicator implements Callable<String> {
 	
-	public HttpCommunicator()
+	private DefaultHttpClient mHttpClient;
+	private HttpParams params;
+	private String URL;
+	
+	public HttpCommunicator(String URL)
 	{
 		super();
+		params = new BasicHttpParams();
+	    params.setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+	    mHttpClient = new DefaultHttpClient(params);
+	    this.URL = URL;
 	}
 
 	@Override
 	public String call() throws Exception {
-		// TODO Auto-generated method stub
+		HttpPost httppost = new HttpPost(URL);
 		return null;
 	}
 
