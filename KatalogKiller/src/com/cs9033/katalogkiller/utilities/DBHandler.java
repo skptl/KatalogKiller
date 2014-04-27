0a8fa851e8e4143e557c92967a8d73ba5743dd61
namanpratyush <np1225@nyu.edu>
2014-04-27 18:13:50 -0400

    Table Created

--- KatalogKiller/src/com/cs9033/katalogkiller/utilities/DBHandler.java
+++ KatalogKiller/src/com/cs9033/katalogkiller/utilities/DBHandler.java
@@ -1,7 +1,7 @@
 package com.cs9033.katalogkiller.utilities;
 
 import android.content.Context;
-import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -12,12 +12,39 @@ public class DBHandler extends SQLiteOpenHelper {
     // Database Version
     private static final int DATABASE_VERSION = 4;
  
+    // Database Name
+    private static final String DATABASE_NAME = "KatalogKillerDB";
+    
+    // Trip table name
+    private static final String TABLE_USER = "UserTable";
+ 
+    // Trip Table Columns names
+    private static final String USER_ID = "user_name";
+    private static final String USER_NAME = "user_name";
+    private static final String USER_EMAIL = "user_email";
+    private static final String USER_PASSWORD = "user_password";
+    private static final String USER_PHONE_NUMBER = "user_phone_number";
+    private static final String USER_ADDRESS = "user_address";
 
 	public DBHandler(Context context, String name, CursorFactory factory,
 			int version) {
 		super(context, name, factory, version);
 		// TODO Auto-generated constructor stub
 	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 
 
