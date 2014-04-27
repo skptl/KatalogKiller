0f7038103772268faece345142119abaf2697418
Shilpan Patel <skp344@nyu.edu>
2014-04-27 02:54:41 -0400

    Manual entry by Naman

--- KatalogKiller/src/com/cs9033/katalogkiller/CameraActivity.java
+++ KatalogKiller/src/com/cs9033/katalogkiller/CameraActivity.java
@@ -1,28 +1,61 @@
 package com.cs9033.katalogkiller;
 
-import com.cs9033.katalogkiller.utilities.Utilities;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import android.app.Activity;
 import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.FrameLayout;
+
+import com.cs9033.katalogkiller.utilities.Utilities;
 
 public class CameraActivity extends Activity {
 
-    private Camera mCamera;
-    private CameraSurfaceView mPreview;
+	private Camera mCamera;
+	private CameraSurfaceView mPreview;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.camera_preview);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.camera_preview);
 
-        // Create an instance of Camera
-        mCamera = Utilities.getCameraInstance();
+		// Create an instance of Camera
+		mCamera = Utilities.getCameraInstance();
 
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraSurfaceView(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
-    }
+		// Create our Preview view and set it as the content of our activity.
+		mPreview = new CameraSurfaceView(this, mCamera);
+		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+		preview.addView(mPreview);
+	}
+
+	private PictureCallback mPicture = new PictureCallback() {
+
+		@Override
+		public void onPictureTaken(byte[] data, Camera camera) {
+
+			File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+			if (pictureFile == null) {
+				Log.d(TAG,
+						"Error creating media file, check storage permissions: "
+								+ e.getMessage());
+				return;
+			}
+
+			try {
+				FileOutputStream fos = new FileOutputStream(pictureFile);
+				fos.write(data);
+				fos.close();
+			} catch (FileNotFoundException e) {
+				Log.d(TAG, "File not found: " + e.getMessage());
+			} catch (IOException e) {
+				Log.d(TAG, "Error accessing file: " + e.getMessage());
+			}
+		}
+	};
+
 }
