e21af23c8db8477aa48292a5f2c2f0f8df4746d5
namanpratyush <np1225@nyu.edu>
2014-04-27 18:55:05 -0400

    Handler change

--- KatalogKiller/src/com/cs9033/katalogkiller/utilities/DBHandler.java
+++ KatalogKiller/src/com/cs9033/katalogkiller/utilities/DBHandler.java
@@ -15,16 +15,37 @@ public class DBHandler extends SQLiteOpenHelper {
     // Database Name
     private static final String DATABASE_NAME = "KatalogKillerDB";
     
-    // Trip table name
+    // User Table table name
     private static final String TABLE_USER = "UserTable";
  
-    // Trip Table Columns names
-    private static final String USER_ID = "user_name";
+    // User Table Columns names
+    private static final String USER_ID = "user_id";
     private static final String USER_NAME = "user_name";
     private static final String USER_EMAIL = "user_email";
     private static final String USER_PASSWORD = "user_password";
     private static final String USER_PHONE_NUMBER = "user_phone_number";
     private static final String USER_ADDRESS = "user_address";
+    
+    // User Subscription table name
+    private static final String TABLE_USER_SUBSCRIBE= "UserSubscribeTable";
+ 
+    // User Subscription Table Columns names
+    private static final String USERID = "user_id";
+    private static final String SUBSCRIPTION_ID = "subscription_id";
+    private static final String SUBSCRIPTION_STATUS = "subscription_status";
+    
+    
+    //Master Subscription table name
+    
+    private static final String TABLE_SUBSCRIPTION= "SubscriptionTable";
+    
+    // Master Subscription Table Columns names
+    private static final String SUBSCRIPTIONID = "subscription_id";
+    private static final String SUBSCRIPTION_NAME = "subscription_name";
+    
+    
+    
+    
 
 	public DBHandler(Context context, String name, CursorFactory factory,
 			int version) {
