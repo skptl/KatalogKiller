5a74cda5ed793cbedd079a78fa81b1b94e3983a4
saijad dhuka <saijaddhuka@gmail.com>
2014-04-27 02:31:48 -0400

    Splash Activity Added

--- KatalogKiller/src/com/cs9033/katalogkiller/SplashActivity.java
+++ KatalogKiller/src/com/cs9033/katalogkiller/SplashActivity.java
@@ -1,24 +1,52 @@
 package com.cs9033.katalogkiller;
 
+
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends Activity {
 	
-	private static String TAG = "SplashActivity";
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.splash);
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	private static String TAG = SplashActivity.class.getName();
+	   private static long SLEEP_TIME = 3;    // Sleep for some time
+
+	   @Override
+	   protected void onCreate(Bundle savedInstanceState) {
+	      super.onCreate(savedInstanceState);
+
+	      this.requestWindowFeature(Window.FEATURE_NO_TITLE);    // Removes title bar
+	      this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);    // Removes notification bar
+
+	      setContentView(R.layout.splash);
+
+	      // Start timer and launch main activity
+	      IntentLauncher launcher = new IntentLauncher();
+	      launcher.start();
+	   }
+
+	   private class IntentLauncher extends Thread {
+	      @Override
+	      /**
+	       * Sleep for some time and than start new activity.
+	       */
+	      public void run() {
+	         try {
+	            // Sleeping
+	            Thread.sleep(SLEEP_TIME*1000);
+	         } catch (Exception e) {
+	            Log.e(TAG, e.getMessage());
+	         }
+
+	         // Start main activity
+	         Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+	         SplashActivity.this.startActivity(intent);
+	         SplashActivity.this.finish();
+	      }
+	   }
 
 }
