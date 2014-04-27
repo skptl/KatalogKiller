f39868ac03c8835ea3e71116203344e22e2e9956
Shilpan Patel <skp344@nyu.edu>
2014-04-27 03:13:01 -0400

    Camera activity from android.

--- KatalogKiller/src/com/cs9033/katalogkiller/CameraActivity.java
+++ KatalogKiller/src/com/cs9033/katalogkiller/CameraActivity.java
@@ -1,23 +1,22 @@
 package com.cs9033.katalogkiller;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import android.app.Activity;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import com.cs9033.katalogkiller.utilities.Utilities;
 
 public class CameraActivity extends Activity {
 
+	private static String TAG = "CameraActivity";
+
 	private Camera mCamera;
 	private CameraSurfaceView mPreview;
+	private Button captureButton;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -31,31 +30,39 @@ public class CameraActivity extends Activity {
 		mPreview = new CameraSurfaceView(this, mCamera);
 		FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
 		preview.addView(mPreview);
+
+		captureButton = (Button) findViewById(R.id.button_capture);
+		captureButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// get an image from the camera
+				mCamera.takePicture(null, null, mPicture);
+			}
+		});
 	}
 
 	private PictureCallback mPicture = new PictureCallback() {
 
 		@Override
 		public void onPictureTaken(byte[] data, Camera camera) {
-
-			File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-			if (pictureFile == null) {
-				Log.d(TAG,
-						"Error creating media file, check storage permissions: "
-								+ e.getMessage());
-				return;
-			}
-
-			try {
-				FileOutputStream fos = new FileOutputStream(pictureFile);
-				fos.write(data);
-				fos.close();
-			} catch (FileNotFoundException e) {
-				Log.d(TAG, "File not found: " + e.getMessage());
-			} catch (IOException e) {
-				Log.d(TAG, "Error accessing file: " + e.getMessage());
-			}
+			System.out.println(new String(data));
+			mCamera.startPreview();
 		}
 	};
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		releaseCamera(); // release the camera immediately on pause event
+	}
+
+	private void releaseCamera() {
+		if (mCamera != null) {
+			mCamera.release(); // release the camera for other applications
+			mCamera = null;
+		}
+	}
+	
+	
 
 }
