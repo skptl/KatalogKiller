03808915eb17090c5d311139a20986d739ff78bb
Shilpan Patel <skp344@nyu.edu>
2014-04-27 19:32:44 -0400

    Redis enabled and made working.

--- KatalogServer/src/main/java/com/cs9033/server/HomeController.java
+++ KatalogServer/src/main/java/com/cs9033/server/HomeController.java
@@ -1,7 +1,5 @@
 package com.cs9033.server;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
--- KatalogServer/src/main/java/com/cs9033/server/utilities/Database.java
+++ KatalogServer/src/main/java/com/cs9033/server/utilities/Database.java
@@ -3,21 +3,20 @@ package com.cs9033.server.utilities;
 import redis.clients.jedis.Jedis;
 
 public class Database {
-	
+
 	private static Jedis jedis;
-	
-	public Database()
-	{
-		if(jedis==null)
-			jedis = new Jedis("127.0.0.1");
-			
-	}
 
 	/**
 	 * @return the jedis
 	 */
-	public Jedis getJedis() {
+	public static Jedis getJedis() {
+		try {
+			if (jedis == null)
+				jedis = new Jedis("127.0.0.1");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return jedis;
-	}	
+	}
 
 }
