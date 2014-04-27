a5fbe159904816e254825fa5a810e8c7e0a7cb80
namanpratyush <np1225@nyu.edu>
2014-04-27 03:29:36 -0400

    Register Activity

--- KatalogKiller/src/com/cs9033/katalogkiller/RegisterActivity.java
+++ KatalogKiller/src/com/cs9033/katalogkiller/RegisterActivity.java
@@ -1,11 +1,22 @@
 package com.cs9033.katalogkiller;
 
+import android.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends Activity {
+	
+	private EditText edtPersonName;
+	private EditText editEmailId;
+	private EditText edtPassword;
+	private EditText edtPhoneNumber;
+	private Button btnRegister;
+	
+	
 
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
