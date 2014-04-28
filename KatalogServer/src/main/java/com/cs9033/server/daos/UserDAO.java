32f702c9b5ecf84b92fa857a3bd0f0b77eb7c228
Shilpan Patel <skp344@nyu.edu>
2014-04-27 22:02:39 -0400

    User DAO updated

--- KatalogServer/src/main/java/com/cs9033/server/daos/UserDAO.java
+++ KatalogServer/src/main/java/com/cs9033/server/daos/UserDAO.java
@@ -7,5 +7,25 @@ import com.cs9033.server.utilities.Database;
 public class UserDAO {
 	
 	private static final Jedis jedis = Database.getJedis();
+	
+	public static int createUser(String json)
+	{
+		return -1;
+	}
+	
+	public static int updateUser(String json)
+	{
+		return -1;
+	}
+	
+	public static int deleteUser(String json)
+	{
+		return -1;
+	}
+	
+	public static int readUser(String json)
+	{
+		return -1;
+	}
 
 }
