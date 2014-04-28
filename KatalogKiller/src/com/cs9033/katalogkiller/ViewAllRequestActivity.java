d99b3edf74e04e382ce017acc489097b6249351b
namanpratyush <np1225@nyu.edu>
2014-04-28 00:17:25 -0400

    VIEW ALL REQUEST TAB ACTIVITY

--- KatalogKiller/src/com/cs9033/katalogkiller/ViewAllRequestActivity.java
+++ KatalogKiller/src/com/cs9033/katalogkiller/ViewAllRequestActivity.java
@@ -1,5 +1,27 @@
 package com.cs9033.katalogkiller;
 
-public class ViewAllRequestActivity {
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
 
+@SuppressWarnings("deprecation")
+public class ViewAllRequestActivity extends TabActivity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewallrequest);
+        TabHost tabHost = getTabHost();       
+      
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("View All Requests").setContent(new Intent(this, ViewAllResultActivity.class)));
+      
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("View Processed Results").setContent(new Intent(this, ViewProcessedResultActivity.class)));
+        
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("View Pending Requests").setContent(new Intent(this, ViewPendingResultActivity.class)));
+        
+        
+        tabHost.setCurrentTab(1); 
+    }
 }
