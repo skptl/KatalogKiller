03b8aac2c9090983f39149cbac44113cb00567a4
Shilpan Patel <skp344@nyu.edu>
2014-04-27 22:03:38 -0400

    Image access updated

--- KatalogServer/src/main/java/com/cs9033/server/daos/ImageDAO.java
+++ KatalogServer/src/main/java/com/cs9033/server/daos/ImageDAO.java
@@ -7,5 +7,25 @@ import com.cs9033.server.utilities.Database;
 public class ImageDAO {
 	
 	private static final Jedis jedis = Database.getJedis();
+	
+	public static int createImage(String json)
+	{
+		return -1;
+	}
+	
+	public static int updateImage(String json)
+	{
+		return -1;
+	}
+	
+	public static int deleteImage(String json)
+	{
+		return -1;
+	}
+	
+	public static int readImage(String json)
+	{
+		return -1;
+	}
 
 }
