eaf184a9973ff2e0630ede77329fca1d4bb1d61a
Shilpan Patel <skp344@nyu.edu>
2014-05-11 00:55:43 -0400

    Multipart File configuration to manage file upload sizes and others.

---
+++ katalog-services/src/main/java/edu/nyu/cs9033/configurations/MultipartConfig.java
@@ -0,0 +1,20 @@
+package edu.nyu.cs9033.configurations;
+
+import javax.servlet.MultipartConfigElement;
+
+import org.springframework.boot.context.embedded.MultiPartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MultipartConfig {
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultiPartConfigFactory factory = new MultiPartConfigFactory();
+		factory.setMaxFileSize("128KB");
+		factory.setMaxRequestSize("128KB");
+		return factory.createMultipartConfig();
+	}
+
+}
