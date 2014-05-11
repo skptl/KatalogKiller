a826a04e65f56336e665ef448e7f802ff19c1fdb
Shilpan Patel <skp344@nyu.edu>
2014-05-11 01:00:08 -0400

    File repository storing binary files in mongodb.

---
+++ katalog-services/src/main/java/edu/nyu/cs9033/repositories/FileRepository.java
@@ -0,0 +1,52 @@
+package edu.nyu.cs9033.repositories;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsOperations;
+import org.springframework.stereotype.Service;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.gridfs.GridFSDBFile;
+import com.mongodb.gridfs.GridFSFile;
+
+@Service
+public class FileRepository {
+
+	@Autowired
+	private GridFsOperations gridOperation;
+
+	public String save(InputStream inputStream, String contentType,
+			String filename) {
+
+		DBObject metaData = new BasicDBObject();
+		metaData.put("file_name", filename);
+		metaData.put("content_type", contentType);
+
+		GridFSFile file = gridOperation.store(inputStream, filename, metaData);
+
+		return file.getId().toString();
+	}
+
+	public GridFSDBFile get(String id) {
+
+		System.out.println("Finding by ID: " + id);
+		return gridOperation.findOne(new Query(Criteria.where("_id").is(
+				new ObjectId(id))));
+	}
+
+	@SuppressWarnings("rawtypes")
+	public List listFiles() {
+		return gridOperation.find(null);
+	}
+
+	public GridFSDBFile getByFilename(String filename) {
+		return gridOperation.findOne(new Query(Criteria.where("filename").is(
+				filename)));
+	}
+}
