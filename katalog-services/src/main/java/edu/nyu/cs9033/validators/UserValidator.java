5f23f3a1812f55662979616d534f6228aabb2b7c
Shilpan Patel <skp344@nyu.edu>
2014-05-11 01:01:30 -0400

    Validator validating user beans.

---
+++ katalog-services/src/main/java/edu/nyu/cs9033/validators/UserValidator.java
@@ -0,0 +1,26 @@
+package edu.nyu.cs9033.validators;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import edu.nyu.cs9033.models.User;
+
+public class UserValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return User.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		ValidationUtils.rejectIfEmpty(errors, "fName", "fName empty.");
+		ValidationUtils.rejectIfEmpty(errors, "lName", "lName empty.");
+		ValidationUtils.rejectIfEmpty(errors, "fName", "fName empty.");
+		ValidationUtils.rejectIfEmpty(errors, "email", "email empty.");
+		ValidationUtils.rejectIfEmpty(errors, "address", "address empty.");
+		ValidationUtils.rejectIfEmpty(errors, "zipCode", "zipCode empty.");
+	}
+
+}
