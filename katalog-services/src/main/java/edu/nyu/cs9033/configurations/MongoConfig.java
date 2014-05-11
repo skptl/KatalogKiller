b4cfb33d21ecc0556005effccbb7d79fb6197eef
Shilpan Patel <skp344@nyu.edu>
2014-05-11 00:54:49 -0400

    MongoDB configurations.

---
+++ katalog-services/src/main/java/edu/nyu/cs9033/configurations/MongoConfig.java
@@ -0,0 +1,43 @@
+package edu.nyu.cs9033.configurations;
+
+import java.net.UnknownHostException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "edu.nyu.cs9033.repositories")
+public class MongoConfig extends AbstractMongoConfiguration {
+
+	private static final String dbName = "katalog-killer";
+	private static final String dbServer = "127.0.0.1";
+	private static final int dbPort = 27017;
+
+	public @Bean
+	MongoTemplate mongoTemplate(Mongo mongo) throws UnknownHostException {
+		return new MongoTemplate(mongo, dbName);
+	}
+
+	public @Bean
+	Mongo mongo() throws UnknownHostException {
+		return new MongoClient(dbServer, dbPort);
+	}
+
+	@Override
+	protected String getDatabaseName() {
+		return dbName;
+	}
+
+	@Bean
+	public GridFsTemplate gridFsTemplate() throws Exception {
+		return new GridFsTemplate(mongoDbFactory(), mappingMongoConverter());
+	}
+
+}
